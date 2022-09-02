5db26781cd62314905356949773b796c4b19d003
syed-ahammad <Syed_Ahammad@outlook.com>
2022-09-02 13:02:54 +0600

    data loaded successfully

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log('js file connect successfully')
+
+// load data from api
+const loadedAllData = async()=>{
+    try{
+        const url = 'https://openapi.programming-hero.com/api/news/categories';
+        const res = await fetch(url);
+        const data = await res.json();
+        const news = data.data;
+        return news; 
+
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+// 
+
+loadedAllData()
